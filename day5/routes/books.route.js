f84b0667dea3deec0f813cd0e6d10e1b259d1995
विनय कार्की <46816702+Binay7587@users.noreply.github.com>
2022-05-20 22:08:24 +0545

    Added day 5, managed separate router file

---
+++ day5/routes/books.route.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    //DB findall
+    // res.status(200).send("Book List");
+    // res.status(200).send(true);
+    res.status(200).send({ name: "Binaya", age: "23" });
+});
+
+router.get('/add', (req, res) => {
+    // res.status(200).send("Book Added.");
+    res.status(200).json({ added: true });
+});
+
+router.get('/delete', (req, res) => {
+    // res.status(200).send("Book Deleted.");
+    res.status(200).json({ deleted: true });
+    
+    // console.log(req.query);
+    // Destructuring the req.query
+    const { id, author } = req.query;
+    console.log(id, author);
+});
+
+export default router;
