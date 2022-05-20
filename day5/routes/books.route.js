b778bd796873b47a83346a194b9faf5f87496f2a
विनय कार्की <46816702+Binay7587@users.noreply.github.com>
2022-05-20 22:17:16 +0545

    Used params in route

--- day5/routes/books.route.js
+++ day5/routes/books.route.js
@@ -14,13 +14,18 @@ router.get('/add', (req, res) => {
     res.status(200).json({ added: true });
 });
 
-router.get('/delete', (req, res) => {
+router.get('/delete/:id/:author', (req, res) => {
     // res.status(200).send("Book Deleted.");
     res.status(200).json({ deleted: true });
     
     // console.log(req.query);
     // Destructuring the req.query
-    const { id, author } = req.query;
+    // const { id, author } = req.query;
+    // console.log(id, author);
+
+    console.log(req.params);
+    // Destructuring the req.params
+     const { id, author } = req.params;
     console.log(id, author);
 });
 
