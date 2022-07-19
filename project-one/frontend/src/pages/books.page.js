4a74121b23c5aaa9f2bc5cb0e27f31774c7ea558
विनय कार्की <46816702+Binay7587@users.noreply.github.com>
2022-07-19 23:56:37 +0545

    Addded: Book delete functionality.

--- project-one/frontend/src/pages/books.page.js
+++ project-one/frontend/src/pages/books.page.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import api from "../api/config";
+import { FaTrashAlt } from "react-icons/fa";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const BooksList = () => {
+  const [bookList, setBookList] = useState([]);
+
+  useEffect(() => {
+    async function getBooks() {
+      const response = await api.get("/books");
+
+      if (response.data) {
+        setBookList(response.data);
+      }
+    }
+
+    getBooks();
+  }, []);
+
+  const deleteBook = async (id, idx) => {
+    const data = window.confirm("Are you sure you want to Delete ?");
+    if (data) {
+      try {
+        const response = await api.delete(`/books/${id}`);
+        if (response.data.success) {
+          const newBookList = bookList.filter((book, index) => index !== idx);
+          setBookList(newBookList);
+
+          toast.success("Book deleted succesfully.");
+        } else {
+          toast.error("Unable to delete the book.");
+        }
+      } catch (err) {
+        toast.error(err.message);
+      }
+    }
+  };
   return (
-    <div>BooksList</div>
-  )
-}
+    <center>
+      <ToastContainer />
+      <h1>Books List</h1>
+      {bookList.length > 0
+        ? bookList.map((book, index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  boxShadow: "0px 0px 5px #ccc",
+                  padding: "10px",
+                  margin: "10px",
+                  color: "green",
+                  width: "45%",
+                  textAlign: "start",
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                {book.name}
+                <FaTrashAlt
+                  color="red"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => deleteBook(book.id, index)}
+                />
+              </div>
+            );
+          })
+        : "No Books"}
+    </center>
+  );
+};
 
 export default BooksList
